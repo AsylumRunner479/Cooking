d5048aed9ba64142ab3cbf2d615a71f6b6a2a186
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-02-11 10:42:18 +1100

    commit

---
+++ New Unity Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Movement : MonoBehaviour
+{
+    [Header("Speed Vars")]
+    //value Variables
+    public float moveSpeed;
+    public float walkSpeed;
+    public static float _gravity = 20;
+    //Struct - Contains Multiple Variables (eg...3 floats)
+    private Vector3 _moveDir;
+    //Reference Variable
+    public PlayerHandler player;
+    public CharacterController _charC;
+
+
+
+    private void Start()
+    {
+        _charC = GetComponent<CharacterController>();
+
+        _gravity = 20;
+
+
+    }
+
+    private void Update()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        if (_charC.isGrounded && !PlayerHandler.isDead)
+        {
+            //set speed
+            
+
+            
+               
+                moveSpeed = walkSpeed;
+
+           
+            if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            {
+
+            }
+            else if (!(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0))
+            {
+
+            }
+            _moveDir = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed);
+           
+        }
+        if (PlayerHandler.isDead)
+        {
+            _moveDir = Vector3.zero;
+        }
+
+        _moveDir.y -= _gravity * Time.deltaTime;
+
+
+        _charC.Move(_moveDir * Time.deltaTime);
+    }
+
+}
+
+
---
+++ New Unity Project/Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerHandler : MonoBehaviour
+{
+    public static bool isDead = false;
+    public float curHealth, maxHealth;
+    public float curStamina, maxStamina;
+    public Slider healthBar;
+    public Slider staminaBar;
+    public GameObject hand;
+    public GameManager manage;
+
+    private void Start()
+    {
+        maxHealth = 100f;
+        maxStamina = 10f;
+        curHealth = maxHealth;
+        curStamina = maxStamina;
+
+        healthBar.value = Mathf.Clamp01(curHealth / maxHealth);
+        staminaBar.value = Mathf.Clamp01(curStamina / maxStamina);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curHealth / maxHealth != healthBar.value)
+        {
+            healthBar.value = Mathf.Clamp01(curHealth / maxHealth);
+        }
+        if (curStamina / maxStamina != staminaBar.value)
+        {
+            staminaBar.value = Mathf.Clamp01(curStamina / maxStamina);
+        }
+
+
+
+        if (Input.GetKey(KeyCode.X))
+        {
+
+            hand.SetActive(true);
+        }
+        else
+        {
+            hand.SetActive(false);
+        }
+        if (curHealth <= 0)
+        {
+            Death();
+        }
+
+
+
+
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+
+
+
+
+
+
+
+
+
+    }
+    void LoseHealth()
+    {
+        curHealth = Mathf.Clamp(curHealth, 0, maxHealth);
+        healthBar.value = Mathf.Clamp01(curHealth / maxHealth);
+
+    }
+
+
+    void Death()
+    {
+
+        isDead = true;
+
+
+
+
+    }
+
+    void Revive()
+    {
+        isDead = false;
+
+
+
+    }
+
+
+
+
+
+
+
+}
+

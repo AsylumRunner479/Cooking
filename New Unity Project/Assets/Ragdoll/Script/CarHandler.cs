08b25dc5a26cc2e4e61be9a8ba3242227c69dd40
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-02-18 15:49:36 +1100

    added car

---
+++ New Unity Project/Assets/Ragdoll/Script/CarHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject self;
+    public HingeJoint[] wheel;
+    // Update is called once per frame
+    void Update()
+    {
+        self.transform.Rotate(0, Input.GetAxis("Horizontal"), 0, 0);
+        if (!(Input.GetAxis("Vertical") <= 0))
+        {
+            for (int i = 0; i < wheel.Length; i++)
+            {
+                JointMotor tempJoint = wheel[i].motor;
+                tempJoint.targetVelocity = 1000f;
+                tempJoint.force += 1f;
+                wheel[i].motor = tempJoint;
+
+
+
+            }
+            
+        }
+        else if (!(Input.GetAxis("Vertical") >= 0))
+        {
+            for (int i = 0; i < wheel.Length; i++)
+            {
+                JointMotor tempJoint = wheel[i].motor;
+                tempJoint.targetVelocity = -1000f;
+                tempJoint.force += 1f;
+                wheel[i].motor = tempJoint;
+            }
+
+        }
+        else
+        {
+            for (int i = 0; i < wheel.Length; i++)
+            {
+                JointMotor tempJoint = wheel[i].motor;
+                tempJoint.force = 0f;
+                wheel[i].motor = tempJoint;
+            }
+        }
+
+    }
+}

a8c065e252f71f98255744deab1459d005e83953
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-02-24 16:27:07 +1100

    added slingshot

---
+++ New Unity Project/Assets/Ragdoll/Script/SlingShotManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShotManager : MonoBehaviour
+{
+    public SpringJoint spring1, spring2;
+    public GameObject self;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (self.transform.localPosition.y >= 6)
+        {
+            Destroy(spring1);
+            Destroy(spring2);
+        }
+    }
+}

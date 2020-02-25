d539874f4c7426ec011ee2ff53b5f8b119b13eb0
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-02-25 14:47:33 +1100

    added ragdoll

--- New Unity Project/Assets/Ragdoll/Script/SlingShotManager.cs
+++ New Unity Project/Assets/Ragdoll/Script/SlingShotManager.cs
@@ -6,6 +6,7 @@ public class SlingShotManager : MonoBehaviour
 {
     public SpringJoint spring1, spring2;
     public GameObject self;
+    public bool launch = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +16,24 @@ public class SlingShotManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (self.transform.localPosition.y >= 6)
+        //if (Input.GetMouseButtonDown(0))
+        //{
+            //launch = false;
+        //}      
+        if (self.transform.localPosition.y >= 10)
         {
-            Destroy(spring1);
-            Destroy(spring2);
+            //Destroy(spring1);
+            //Destroy(spring2);
+            //launch = true;
+        }
+        else
+        {
+//if (Input.GetMouseButtonDown(0) && launch == false)
+            //{
+               // Vector3 pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+               // self.transform.position = pos;
+           // }
+            
         }
     }
 }
